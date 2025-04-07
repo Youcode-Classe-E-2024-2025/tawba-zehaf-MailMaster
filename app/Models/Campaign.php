f4a026ebc842ef66cf74a14dc702a49bc8d40c64
tawbazf <tawbazehaf@gmail.com>
2025-04-07 16:30:55 +0100

    campaign  model

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -6,5 +6,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Campaign extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'newsletter_id',
+        'sent_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'sent_at' => 'datetime',
+    ];
+
+    /**
+     * Get the newsletter associated with the campaign.
+     */
+    public function newsletter()
+    {
+        return $this->belongsTo(Newsletter::class);
+    }
 }
