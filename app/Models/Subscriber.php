540f11168d7d9ab3fd5e14a5547c73751e42df6d
tawbazf <tawbazehaf@gmail.com>
2025-04-07 16:29:25 +0100

    subscriber  model

--- app/Models/Subscriber.php
+++ app/Models/Subscriber.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Subscriber extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'email',
+        'subscribed',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'subscribed' => 'boolean',
+    ];
 }
